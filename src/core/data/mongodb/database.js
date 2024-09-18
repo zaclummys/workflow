471f41f25691e7af99ee985383ee5245e076c48e
Isaac Ferreira <zaclummys@gmail.com>
2024-09-18 20:57:08 -0300

    Improve database testing

--- src/core/data/mongodb/database.js
+++ src/core/data/mongodb/database.js
@@ -4,38 +4,22 @@ import client from './client';
 
 const database = client.db('workflow');
 
+
+
 const users = database.collection('users');
 const sessions = database.collection('sessions');
 const workspaces = database.collection('workspaces');
+const workflowVersions = database.collection('workflow-versions');
+const workflowExecutions = database.collection('workflow-executions');
 
-users.createIndex({
-    id: 1,
-}, {
-    unique: true,
-});
+users.createIndex({ id: 1, }, { unique: true });
+users.createIndex({ email: 1 }, { unique: true });
 
-users.createIndex({
-    email: 1,
-}, {
-    unique: true,
-});
+sessions.createIndex({ id: 1 }, { unique: true });
+sessions.createIndex({ token: 1 }, { unique: true });
 
-sessions.createIndex({
-    id: 1,
-}, {
-    unique: true,
-});
-
-sessions.createIndex({
-    token: 1,
-}, {
-    unique: true,
-});
-
-workspaces.createIndex({
-    id: 1,
-}, {
-    unique: true,
-});
+workspaces.createIndex({ id: 1 }, { unique: true });
+workflowVersions.createIndex({ id: 1 }, { unique: true });
+workflowExecutions.createIndex({ id: 1 }, { unique: true });
 
 export default database;
---
+++ tests/core/data/mongodb/database.test.js
@@ -0,0 +1,39 @@
+import database from "~/core/data/mongodb/database";
+
+describe('MongoDB Database', () => {
+    it('Should ping to DB', async () => {
+        const { ok } = await database.command({
+            ping: 1
+        });
+       
+        expect(ok).toBe(1);
+    });
+    
+    it.each([
+        ['users', [
+            { id: 1 },
+            { email: 1 },
+        ]],
+        
+        ['sessions', [
+            { id: 1 },
+            { token: 1 },
+        ]],
+        ['workspaces', [{ id: 1 }]],
+        ['workflow-versions', [{ id: 1 }]],
+        ['workflow-executions', [{ id: 1 }]],
+    ])('Collection %s should have unique indexes', async (collectionName, uniqueIndexKeys) => {
+        const indexes = await database.collection(collectionName).listIndexes().toArray();
+        
+        expect(indexes).toStrictEqual(
+            expect.arrayContaining(
+                uniqueIndexKeys.map(uniqueIndexKey => (
+                    expect.objectContaining({
+                        key: uniqueIndexKey,
+                        unique: true,
+                    })
+                ))
+            )
+        );
+    })
+});
