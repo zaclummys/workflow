1e1902ee69a7b490511ecec31fd5052c7be158ba
Isaac Ferreira <zaclummys@gmail.com>
2024-12-01 21:38:31 -0300

    Rename navigation function for opening workflow version editor

--- src/components/open-workflow-version-editor-button.js
+++ src/components/open-workflow-version-editor-button.js
@@ -4,11 +4,11 @@ import useNavigation from '~/hooks/use-navigation';
 import { PrimaryButton } from '~/components/button';
 
 export default function OpenWorkflowVersionEditorButton ({ workflowVersionId }) {
-    const { navigateToWorkflowVersionEditor } = useNavigation();
+    const { openWorkflowVersionEditor } = useNavigation();
     
     return (
         <PrimaryButton
-            onClick={() => navigateToWorkflowVersionEditor(workflowVersionId)}>
+            onClick={() => openWorkflowVersionEditor(workflowVersionId)}>
             Open
         </PrimaryButton>
     );
